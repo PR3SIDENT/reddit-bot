f8767202a841ff50e9d8d297184fe7fdd2b1d4b5
Cory Niblett <cory@niblett.me>
2024-03-05 14:41:02 -0600

    remove discord embed previews
    Escaping URLs with markdown.

--- bot.py
+++ bot.py
@@ -94,7 +94,7 @@ def sticky_comment_on_whitelisted_user_post():
                 existing_sticky.edit(sticky_comment_text_with_comment)
                 print(f"Edited a comment on post: {submission.title}")
                 # Send notification to Discord
-                send_to_discord(f"A stickied reply has been updated in r/{subreddit_name} by {comment.author.name}. [Link to comment](https://www.reddit.com{comment.permalink})")
+                send_to_discord(f"A stickied reply has been updated in r/{subreddit_name} by {comment.author.name}. [Link to comment](<https://www.reddit.com{comment.permalink}>)")
                 
             # Else, create new comment and sticky
             else:
@@ -112,7 +112,7 @@ def sticky_comment_on_whitelisted_user_post():
                 bot_sticky_comment.mod.distinguish(how='yes', sticky=True)
                 print(f"Stickied a comment on post: {submission.title}\nPost id: {submission.id}\nComment id: {bot_sticky_comment.id}")
                 # Send notification to Discord
-                send_to_discord(f"A stickied reply has been posted in r/{subreddit_name} by {comment.author.name}. [Link to comment](https://www.reddit.com{comment.permalink})")
+                send_to_discord(f"A stickied reply has been posted in r/{subreddit_name} by {comment.author.name}. [Link to comment](<https://www.reddit.com{comment.permalink}>)")
 
 while True:
     try:
