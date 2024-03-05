b7a567f64bf73a47244c3b5aafe4f0fb1b8154fb
JeffP <jeffp@jeffpham.com>
2024-03-05 10:36:07 -0500

    try/except to keep connected on error

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import ast
+import time
 from dotenv import load_dotenv
 import praw
 from sqlitedict import SqliteDict
@@ -33,6 +34,8 @@ bot_user = reddit.user.me()
 
 # Subreddit to monitor
 subreddit_name = os.environ["SUBREDDIT"]
+subreddit = reddit.subreddit(subreddit_name)
+stream = subreddit.stream.comments(skip_existing=True)
 
 # The text for the sticky comment
 sticky_comment_text = """
@@ -43,47 +46,51 @@ This post has comments from approved developers. Their replies can be found here
 def sticky_comment_on_whitelisted_user_post():
     print(f"Bot started and listening in r/{subreddit_name}")
     print(f"Whitelisted users: {whitelisted_users_lower}")
-    subreddit = reddit.subreddit(subreddit_name)
-    for comment in subreddit.stream.comments(skip_existing=True):
-        # Check if new comment is from a whitelisted username
-        if comment.author and (comment.author.name.lower() in whitelisted_users_lower):
-            print(f"Found a post by whitelisted user: {comment.author.name}")
-            submission = comment.submission
-            existing_sticky = None
-            
-            # Check if post already has developer comments bot sticky
-            if submission.id in db.keys():
-                existing_sticky = reddit.comment(db[submission.id])
-                print(f"Found bot post id: {existing_sticky.id}")
-            
-            if existing_sticky:
-                sticky_body = existing_sticky.body
-                # Append comment to template
-                comment_body:str = comment.body
-                comment_body_split = comment_body.split()
-                # Shorten comment to first 5 words if longer
-                if (len(comment_body_split) > 5):
-                    comment_body = " ".join(comment_body_split[:5]) + " ..."
-                sticky_comment_text_with_comment = f"{sticky_body}\n\n/u/{comment.author.name} posted a comment: [{comment_body}]({comment.permalink})"
-                # Edit sticky comment with new post
-                existing_sticky.edit(sticky_comment_text_with_comment)
-                print(f"Edited a comment on post: {submission.title}")
-                
-            # Else, create new comment and sticky
-            else:
-                # Append comment to template
-                comment_body:str = comment.body
-                comment_body_split = comment_body.split()
-                # Shorten comment to first 5 words if longer
-                if (len(comment_body_split) > 5):
-                    comment_body = " ".join(comment_body_split[:5]) + " ..."
-                sticky_comment_text_with_comment = f"{sticky_comment_text}\n\n/u/{comment.author.name} posted a comment: [{comment_body}]({comment.permalink})"
-                # Post the comment
-                bot_sticky_comment = submission.reply(sticky_comment_text_with_comment)
-                db[submission.id] = bot_sticky_comment.id
-                # Sticky and distinguish the comment
-                bot_sticky_comment.mod.distinguish(how='yes', sticky=True)
-                print(f"Stickied a comment on post: {submission.title}\nPost id: {submission.id}\nComment id: {bot_sticky_comment.id}")
+    while True:
+        try:
+            for comment in stream:
+                # Check if new comment is from a whitelisted username
+                if comment.author and (comment.author.name.lower() in whitelisted_users_lower):
+                    print(f"Found a post by whitelisted user: {comment.author.name}")
+                    submission = comment.submission
+                    existing_sticky = None
+                    
+                    # Check if post already has developer comments bot sticky
+                    if submission.id in db.keys():
+                        existing_sticky = reddit.comment(db[submission.id])
+                        print(f"Found bot post id: {existing_sticky.id}")
+                    
+                    if existing_sticky:
+                        sticky_body = existing_sticky.body
+                        # Append comment to template
+                        comment_body:str = comment.body
+                        comment_body_split = comment_body.split()
+                        # Shorten comment to first 5 words if longer
+                        if (len(comment_body_split) > 5):
+                            comment_body = " ".join(comment_body_split[:5]) + " ..."
+                        sticky_comment_text_with_comment = f"{sticky_body}\n\n/u/{comment.author.name} posted a comment: [{comment_body}]({comment.permalink})"
+                        # Edit sticky comment with new post
+                        existing_sticky.edit(sticky_comment_text_with_comment)
+                        print(f"Edited a comment on post: {submission.title}")
+                        
+                    # Else, create new comment and sticky
+                    else:
+                        # Append comment to template
+                        comment_body:str = comment.body
+                        comment_body_split = comment_body.split()
+                        # Shorten comment to first 5 words if longer
+                        if (len(comment_body_split) > 5):
+                            comment_body = " ".join(comment_body_split[:5]) + " ..."
+                        sticky_comment_text_with_comment = f"{sticky_comment_text}\n\n/u/{comment.author.name} posted a comment: [{comment_body}]({comment.permalink})"
+                        # Post the comment
+                        bot_sticky_comment = submission.reply(sticky_comment_text_with_comment)
+                        db[submission.id] = bot_sticky_comment.id
+                        # Sticky and distinguish the comment
+                        bot_sticky_comment.mod.distinguish(how='yes', sticky=True)
+                        print(f"Stickied a comment on post: {submission.title}\nPost id: {submission.id}\nComment id: {bot_sticky_comment.id}")
+        except Exception as error:
+            print(f"ERROR:\n{error}")
+        time.sleep(60)
 
 # Run the bot
 sticky_comment_on_whitelisted_user_post()
